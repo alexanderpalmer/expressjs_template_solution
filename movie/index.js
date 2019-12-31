87cea15144de65a2bb1677b04e9c58375b236d56
Alexander Palmer <apalmer@zbw.ch>
2019-12-31 14:27:57 +0100

    Init Moviedatabase

---
+++ movie/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+const data = [
+  {id:1, title: 'Iron Man', year: '2008'},
+  {id:2, title: 'Thor', year: '2011'},
+  {id:3, title: 'Captain America', year: '2011'}
+];
+
+router.get('/', (request, response) => {
+  response.send(data);
+})
+
+module.exports = router;
